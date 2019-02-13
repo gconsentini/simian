bb8bf4fb946376581ef2db8356fb708314c7f1ee
Gustavo Consentini <gustavoconsentini@gmail.com>
2019-02-13 16:08:43 -0200

    adicionando documentacao da api na controller

--- src/main/java/com/gconsentini/simian/controller/SimianController.java
+++ src/main/java/com/gconsentini/simian/controller/SimianController.java
@@ -5,6 +5,9 @@ package com.gconsentini.simian.controller;
 import com.gconsentini.simian.model.dto.SimianDto;
 import com.gconsentini.simian.model.dto.StatsDto;
 import com.gconsentini.simian.service.SimianService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,14 +25,21 @@ public class SimianController {
     @Autowired
     private SimianService simianService;
 
+    @ApiOperation(value = "Verifica se um DNA é simio")
     @PostMapping("/simian")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 403, message = "Forbidden")
+    })
     public ResponseEntity isSimian(@RequestBody SimianDto simianDto){
         if (!simianService.isSimian(simianDto))
             return new ResponseEntity(HttpStatus.FORBIDDEN);
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Verifica os status de todos os dnas verificados", response = StatsDto.class)
     @GetMapping("/stats")
+    @ApiResponse(code = 200, message = "Success", response = StatsDto.class)
     public StatsDto getStats(){
         return simianService.getStats();
     }
